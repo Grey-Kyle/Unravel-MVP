82797c4966cf0694c83bb35657b67f467edb23a0
Kyle Grey <aggreykyle@gmail.com>
2026-08-02 13:39:13 +0300

    fix express 5 wildcard route

--- backend/server.js
+++ backend/server.js
@@ -305,7 +305,7 @@ app.get('/api/user/profile', authenticateToken, (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 

fd18566f9b3f6701d177c8ffc56e26a3fcec113f
Kyle Grey <aggreykyle@gmail.com>
2026-08-08 15:08:04 +0300

    remove unused Profile import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import AdminDashboard from './AdminDashboard';
-import Profile from './Profile';
 import './App.css';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
